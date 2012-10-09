423b8bb35cb15d51bff199410d44b535c61784f3
Tran Cong Hoang <hoang.trancong90@gmail.com>
2012-10-09 15:07:19 +0800

    rollback to creating user

---
+++ app/models/ability.rb
@@ -0,0 +1,50 @@
+class Ability
+  include CanCan::Ability
+ 
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user permission to do.
+    # If you pass :manage it will apply to every action. Other common actions here are
+    # :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on. If you pass
+    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+
+    user ||= User.new # guest user
+ 
+    puts "RUNNING HERE ", user.name, user.id
+    if user.role? :super_admin
+      can :manage, :all
+    elsif user.role? :admin
+#      can :manage, [Product, Asset, Issue]
+      can :see_timestamps, User
+    elsif user.role? :normal
+      can :manage, User, :id => user.id
+      # manage projects he owns
+      can :manage, Item do |item|
+        item.try(:owner) == user
+      end
+      can :manage, Project do |project|
+        project.try(:owner) == user
+      end
+    else 
+      # all user can read other user's profile, not change
+      can :read, User
+    end
+  end
+end
---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :item do
+    title "MyString"
+    description "MyString"
+    item_url "MyString"
+    category ""
+  end
+end
