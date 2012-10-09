01e13049ab78f23d5128f85399462be3d7f5ecaf
Amulya Khare <amulyakhare@gmail.com>
2012-10-09 17:04:50 +0800

    updated backbone and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap
 //= require underscore
 //= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/describe_me
+//= require underscore
+//= require backbone
+//
+//= require .//describe_me
+//
+//= require_tree ../templates/
+//= require_tree .//models
+//= require_tree .//collections
+//= require_tree .//views
+//= require_tree .//routers
 //= require_tree .
---
+++ app/assets/javascripts/describe_me.js
@@ -0,0 +1,13 @@
+window.DescribeMe = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  DescribeMe.init();
+});
