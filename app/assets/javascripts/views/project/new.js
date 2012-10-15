e7696bc998dab53184e846645fcdf6c4d007e336
Amulya Khare <amulyakhare@gmail.com>
2012-10-15 19:07:59 +0800

    upload image works

--- app/assets/javascripts/views/project/new.js
+++ app/assets/javascripts/views/project/new.js
@@ -9,6 +9,7 @@ DescribeMe.Views.ProjectNew = Backbone.View.extend({
 
 	events : {
 		'submit form' : 'onSave',
+		'change #img-uploader': 'uploadImage'
 	},
 
 	onSave: function() {
@@ -33,9 +34,25 @@ DescribeMe.Views.ProjectNew = Backbone.View.extend({
 		
 	},
 
+	onUploadComplete: function(img_url, message) {
+		$('#img-upload-preview').attr('src', img_url);
+	},
+
+	uploadImage: function() {
+		var input = document.getElementById('img-uploader');
+		if (input.files && input.files[0]) {
+         	$('#img-upload-preview').attr('src', 'http://i293.photobucket.com/albums/mm49/oficinademultimedia/Loading_Animation.gif');
+            Upload.doUpload(input.files[0], this.onUploadComplete);
+        }
+	},
+
 	render: function() {
 		$(this.el).html(this.template());
 
+		this.imageUploadModal = $(this.el).find('#imageUploadModal');
+		this.imageUploadModal.modal();
+		this.imageUploadModal.modal('hide');
+		
 		this.titleDOM = $(this.el).find('#title');
 		this.descriptionDOM = $(this.el).find('#description');
 		this.tagDOM = $(this.el).find('#tag');
