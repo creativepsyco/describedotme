8119426cf802b1126a775f1e3c8c6afa7b8b290d
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-11-12 20:13:50 +0800

    Reverting Bootstrap version

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.8'
 
 gem 'mysql2'
 group :assets do
+  #gem 'bootstrap-sass', '~> 2.1.1.0'
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,7 @@ module DescribeMe
 
     config.active_record.observers = [:item_observer, :comment_observer, :favorite_kudo_item_observer, 
                                 :user_follow_user_observer]
+
+    config.assets.initialize_on_precompile = false
   end
 end
